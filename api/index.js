5188fc36759d31859422339564cb22e5f4558243
Amit kumar <93227051+Amitkr001@users.noreply.github.com>
2024-05-07 18:39:03 +0530

    created mongodb database and connected succesfully

--- api/index.js
+++ api/index.js
@@ -1,10 +1,15 @@
 import express from 'express';
-import mangodb from 'mongodb';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log('Connected to MongoDB');
+});
 
 const app = express();
 
-
-
 app.listen(3001, () => {
   console.log('Server is running on port 3001');
-})
+});
