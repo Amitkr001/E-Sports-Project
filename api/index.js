803eac2f3516b76891377020fb68d414fb454b48
Amit kumar <93227051+Amitkr001@users.noreply.github.com>
2024-05-07 19:43:14 +0530

    controllers and routes done for the test api

--- api/index.js
+++ api/index.js
@@ -1,15 +1,22 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
+
+const app = express();
+
+// Middleware to parse JSON bodies
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO).then(() => {
   console.log('Connected to MongoDB');
 });
 
-const app = express();
-
 app.listen(3001, () => {
   console.log('Server is running on port 3001');
 });
+
+// Use userRoutes for handling requests starting with /api/users
+app.use('/api/users', userRoutes);
