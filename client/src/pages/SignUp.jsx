ae5176bf56c1f2684d1f58a232eebaf57fe8aa97
Amit kumar <93227051+Amitkr001@users.noreply.github.com>
2024-05-09 23:15:35 +0530

    ui of signUp page is don on replit but not implimented

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -4,14 +4,7 @@ export default function SignIn() {
   return (
       //neede to devide page in two section right and left .left side for form and right side for image
     <div className='min-h-screen mt-20'>
-      <div className=''>
-        //div for left side of the page
-        
-
-      </div>
-      <div>
-        //div for right side of the page
-      </div>
+      //signup page done on repl it 
       
     </div>
   )
