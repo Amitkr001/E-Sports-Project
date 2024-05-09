e9edfc5269aab5318607ad9c4ad4b6cc1e629a61
Amit kumar <93227051+Amitkr001@users.noreply.github.com>
2024-05-09 18:57:08 +0530

    created left and right div  for signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
-export default function SignUp() {
+export default function SignIn() {
   return (
-    <div>
-      //next 
+      //neede to devide page in two section right and left .left side for form and right side for image
+    <div className='min-h-screen mt-20'>
+      <div className=''>
+        //div for left side of the page
+        
+
+      </div>
+      <div>
+        //div for right side of the page
+      </div>
+      
     </div>
   )
 }
