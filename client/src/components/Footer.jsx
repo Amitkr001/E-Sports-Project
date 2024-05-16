0ac2e745a96d569883d98480b163ed91f0be1e75
Amit kumar <93227051+Amitkr001@users.noreply.github.com>
2024-05-16 07:49:19 +0530

    footer section completed

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,47 +1,56 @@
-import React from 'react'
+import React from 'react';
 import { Footer } from "flowbite-react";
+import { FaGithub, FaTwitter, FaFacebook } from 'react-icons/fa';
 import logoImg from "../assets/logo.png";
 
 export function FooterComponent() {
   return (
-    <Footer container className='border border-t-8 '>
-        <div className='w-full max-w-7xl mx-auto'>
-        <div className='grid w-full justify-between sm:flex sm:justify-between md:grid-cols-1'>
-            <div className='logo'>
-            <img
-          src={logoImg}
-          alt="logo"
-          className="h-16 w-16"
-          style={{ filter: "drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.2))" }}
-        />
-            </div>
-            <div className='grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6'>
-                <div className=''>
-                    <Footer.Title title='About'/>
-                    <Footer.Link href='#' target='_blank' rel='noopener noreferrer'>GitHub</Footer.Link>
-                    <Footer.Link href='#' target='_blank' rel='noopener noreferrer'>GitHub</Footer.Link>
-                    <Footer.Link href='#' target='_blank' rel='noopener noreferrer'>GitHub</Footer.Link>
-                </div>
-                <div>
-                    <Footer.Title title='About'/>
-                    <Footer.Link href='#' target='_blank' rel='noopener noreferrer'>GitHub</Footer.Link>
-                    <Footer.Link href='#' target='_blank' rel='noopener noreferrer'>GitHub</Footer.Link>
-                    <Footer.Link href='#' target='_blank' rel='noopener noreferrer'>GitHub</Footer.Link>
-                </div>
-                <div>
-                    <Footer.Title title='About'/>
-                    <Footer.Link href='#' target='_blank' rel='noopener noreferrer'>GitHub</Footer.Link>
-                    <Footer.Link href='#' target='_blank' rel='noopener noreferrer'>GitHub</Footer.Link>
-                    <Footer.Link href='#' target='_blank' rel='noopener noreferrer'>GitHub</Footer.Link>
-                </div>
-            </div>
-            <Footer.Divider/>
-            <div className='icons'></div>
-            
+    <Footer container className='border border-t-8'>
+      <div className='container mx-auto py-6 px-4 sm:px-6 lg:px-8'>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-8'>
+          {/* Important Links */}
+          <div className="md:col-span-1">
+            <Footer.Title title='Important Links'/>
+            <ul className="list-disc list-inside mt-2">
+              <li className="mb-2 list-none"><Footer.Link href='#' target='_blank' rel='noopener noreferrer'>Portfolio</Footer.Link></li>
+              <li className="mb-2 list-none"><Footer.Link href='#' target='_blank' rel='noopener noreferrer'>How To Join</Footer.Link></li>
+              <li className="mb-2 list-none"><Footer.Link href='#' target='_blank' rel='noopener noreferrer'>How to use </Footer.Link></li>
+            </ul>
+          </div>
+
+          {/* Policies */}
+          <div className="md:col-span-1">
+            <Footer.Title title='Policies'/>
+            <ul className="list-disc list-inside mt-2">
+              <li className="mb-2 list-none"><Footer.Link href='#' target='_blank' rel='noopener noreferrer'>Privacy Policy</Footer.Link></li>
+              <li className="mb-2 list-none"><Footer.Link href='#' target='_blank' rel='noopener noreferrer'>Terms of Service</Footer.Link></li>
+              <li className="mb-2 list-none"><Footer.Link href='#' target='_blank' rel='noopener noreferrer'>Cookie Policy</Footer.Link></li>
+            </ul>
+          </div>
+
+          {/* Social Media Icons */}
+          <div className='md:col-span-1'>
+            <Footer.Title title='Follow Us'/>
+            <ul className="flex justify-center md:justify-end items-center list-none">
+              <li className="mr-4"><a href="#" target="_blank" rel="noopener noreferrer"><FaGithub size={24} /></a></li>
+              <li className="mr-4"><a href="#" target="_blank" rel="noopener noreferrer"><FaTwitter size={24} /></a></li>
+              <li><a href="#" target="_blank" rel="noopener noreferrer"><FaFacebook size={24} /></a></li>
+            </ul>
+          </div>
         </div>
-        </div>
-      
+
+        {/* Navbar */}
+        <nav className="mt-6 ">
+          <ul className="flex justify-center md:justify-end items-center list-none mt-2">
+            <li className="mr-6"><a href="/">Home</a></li>
+            <li className="mr-6"><a href="/about">About</a></li>
+            <li className="mr-6"><a href="/events">events</a></li>
+            <li className="mr-6"><a href="/blogs">blogs</a></li>
+          </ul>
+        </nav>
+      </div>
     </Footer>
   );
 }
+
 export default FooterComponent;
