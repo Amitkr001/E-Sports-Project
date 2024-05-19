8365e2621ad919dd60e511ddd9aa64b4fbb059a9
Amit kumar <93227051+Amitkr001@users.noreply.github.com>
2024-05-20 04:22:26 +0530

    ninor changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import { store } from './redux/store.js'
 import { Provider } from'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
 )
